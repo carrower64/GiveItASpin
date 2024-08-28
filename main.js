0774905b3acc6672de652348d11810657ffb377a
carrower64 <prathmeshgupta.12th@gmail.com>
2024-08-28 19:54:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -82,7 +82,6 @@ window.addEventListener("resize", () => {
 
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(sphere.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 });
-tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
 
 let mouseDown = false;
